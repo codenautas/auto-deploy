bd9bdfb974b8fbce2249d2742926df9fe3a779e6
Diego F.(W) <diegoefe@gmail.com>
2015-06-23 11:48:03 -0300

    Independizo el package.json utilizado en los tests del real

--- test/test.js
+++ test/test.js
@@ -8,40 +8,62 @@ var expectCalled = require('expect-called');
 var autoDeploy = require('../auto-deploy.js');
 
 describe('auto-deploy', function(){
-  describe('initialization', function(){
-    it('must read auto-deploy section from package.json', function(done){
-        return autoDeploy.initVars().then(function(vars) {
-            //console.log(vars);
-            expect(vars).to.eql({
-                "commands": {
-                    "reset": "git reset --hard",
-                    "restart": "nop",
-                    "stop": "exit",
-                    "update": "git pull"
-                },
-                "server": "node example/server.js",
-                "param": {"pid" : "3344"}
+    var pkgJS = {
+        "auto-deploy": {
+            "server": "node example/server.js",
+            "log": true,
+            "logFile": "./server.log",
+            "commands": {
+                "update": "git pull",
+                "reset": "git reset --hard",
+                "restart": "nop",
+                "stop": "exit"
+            },            
+            "param": {"pid" : "3344"}
+            }
+        };
+    var controlReadJSon;
+    before(function(done){
+        controlReadJSon = expectCalled.control(fs,'readJson',{returns:[
+            Promises.Promise.resolve(pkgJS),
+            Promises.Promise.resolve(pkgJS),
+            Promises.Promise.resolve(pkgJS)
+        ]});
+        done();
+    });
+    after(function(done){
+        controlReadJSon.stopControl();
+        done();
+    });
+    describe('initialization', function(){
+        it('must read auto-deploy section from package.json', function(done){
+            autoDeploy.initVars().then(function(vars) {
+                //console.log(vars);
+                var adp=pkgJS['auto-deploy'];
+                expect(vars.commands).to.eql(adp.commands);
+                expect(vars.server).to.eql(adp.server);
+                expect(vars.param).to.eql(adp.param);
+                done();
+            }).catch(function(err) {
+                console.log("ERROR", err);
+                console.log("STACK", err.stack);
+                controlReadJSon.stopControl();
+                done(err);
             });
-            done();
-        }).catch(function(err) {
-            console.log("ERROR", err);
-            console.log("STACK", err.stack);
-            done(err);
         });
-    });
-    it('must set autoDeploy vars', function(done){
-        return autoDeploy.initVars().then(function(vars) {
-            //console.log(autoDeploy);
-            expect(autoDeploy.childPID).to.eql(0);
-            expect(autoDeploy.fOut).to.be.above(process.stderr.fd);
-            expect(autoDeploy.fErr).to.be.above(process.stderr.fd);
-            done();
-        }).catch(function(err) {
-            console.log("ERROR", err);
-            console.log("STACK", err.stack);
-            done(err);
+        it('must set autoDeploy vars', function(done){
+            return autoDeploy.initVars().then(function(vars) {
+                //console.log(autoDeploy);
+                expect(autoDeploy.childPID).to.eql(0);
+                expect(autoDeploy.fOut).to.be.above(process.stderr.fd);
+                expect(autoDeploy.fErr).to.be.above(process.stderr.fd);
+                done();
+            }).catch(function(err) {
+                console.log("ERROR", err);
+                console.log("STACK", err.stack);
+                done(err);
+            });
         });
-    });
   });
   describe('utility methods', function(){
     it('must generate links for predefined actions', function(done){
